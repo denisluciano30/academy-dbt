778dd4b3e6c599f1b9469a12f6bd20457bc92482
denis.lopes3012@gmail.com <denis.lopes3012@gmail.com>
2023-02-27 23:50:16 +0000

    stg de cartão e produtos

---
+++ models/staging/stg_erp__creditcard.sql
@@ -0,0 +1,12 @@
+with source_creditcard as (
+    select
+        cast(creditcardid as int) as cartao_credito_id,
+	    cast(cardtype as string) as cartao_credito_tipo,
+	    cast(cardnumber as int) as numero_cartao,
+	    cast(expmonth as int) as mes_expiracao,
+	    cast(expyear as int) as ano_expiracao
+	    --modifieddate
+    
+    from {{source('erp','creditcard')}}
+)
+select * from source_creditcard
