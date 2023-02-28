70da50edbc5599ae4761d1c3dda37577bb4c8869
denis.lopes3012@gmail.com <denis.lopes3012@gmail.com>
2023-02-28 01:38:43 +0000

    Padronização e ajustes

--- models/staging/stg_erp__person.sql
+++ models/staging/stg_erp__person.sql
@@ -1,7 +1,7 @@
 with source_person as (
     select
-        businessentityid as pessoa_id,
-        persontype as tipo_pessoa,
+        cast(businessentityid as int) as pessoa_id,
+        cast(persontype as string) as tipo_pessoa,
         cast(firstname as string) as primeiro_nome,
         cast(middlename as string) as nome_meio,
         cast(lastname as string) as ultimo_nome
