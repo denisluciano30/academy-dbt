18e28a0aef66034866537ac459bf7ff4a03fdc32
denis.lopes3012@gmail.com <denis.lopes3012@gmail.com>
2023-03-07 23:26:11 +0000

    Não estava considerando que alguns clientes são lojas

--- models/marts/dim_clientes.sql
+++ models/marts/dim_clientes.sql
@@ -2,7 +2,8 @@ with clientes as (
     SELECT 
         cliente_id,
         pessoa_id,
-        regiao_id
+        regiao_id,
+        loja_id
     FROM {{ ref('stg_erp__customer') }}
 ), pessoas as (
     SELECT 
@@ -12,12 +13,21 @@ with clientes as (
         nome_meio,
         ultimo_nome
     FROM {{ ref('stg_erp__person') }}
+), lojas as (
+    select
+        loja_id,
+        nome
+    from {{ ref('stg_erp__store') }}
 )
 select 
     c.cliente_id,
     c.regiao_id,
     p.pessoa_id,
-    CONCAT(primeiro_nome, ' ', nome_meio, ' ', ultimo_nome) AS nome
+    (case
+        when c.loja_id is not null then l.nome
+        else CONCAT(primeiro_nome, ' ', nome_meio, ' ', ultimo_nome)
+    end) as nome
 
 from clientes c
 left join pessoas p on c.pessoa_id = p.pessoa_id
+left join lojas l on c.loja_id = l.loja_id
