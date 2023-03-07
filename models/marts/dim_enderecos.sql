e8cda1dbe12389b56de8d3fb62a902ba1caa8654
denis.lopes3012@gmail.com <denis.lopes3012@gmail.com>
2023-03-07 02:11:25 +0000

    Novas transformações de endereço + Correções

--- models/marts/dim_enderecos.sql
+++ models/marts/dim_enderecos.sql
@@ -7,8 +7,34 @@ WITH enderecos AS (
         endereco2,
         CEP
     FROM {{ ref('stg_erp__address') }}
-) 
+), estado as (
+    select 
+        estado_id,
+        codigo_estado,
+        codigo_regiao,
+        nome,
+        territorio_id
+    from {{ ref('stg_erp__stateprovince') }}
+), pais as (
+    select 
+        codigo_regiao,	
+        nome
+    from {{ ref('stg_erp__countryregion') }} 
+)
 SELECT
-    *
-FROM enderecos
+    en.endereco_id,
+    en.estado_id,
+    en.cidade, 
+    en.endereco1 as endereco,
+    en.CEP,
+    es.codigo_estado,
+    es.codigo_regiao,
+    es.nome as estado,
+    es.territorio_id,
+    p.nome as pais
+
+
+FROM enderecos en
+left join estado es on en.estado_id = es.estado_id
+left join pais p on es.codigo_regiao = p.codigo_regiao
 
